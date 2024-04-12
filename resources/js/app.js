87679048fbe43dce3b4bcaac6db14d4682558893
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-12 16:44:12 +0200

    exercises completed with bonus

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import "./bootstrap";
 
 import "~resources/scss/app.scss";
-
+import "~bootstrap/js/index.esm.js";
 import.meta.glob(["../img/**"]);
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -25,7 +25,41 @@
                 <img src="{{Vite::asset('resources/img/adv.jpg')}}" alt="">
             </div>
         </div>
-        
+
+
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered ">
+              <div class="modal-content">
+    
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina la pasta</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+    
+                <div class="modal-body">
+                  Are you sure to delete {{$comic->title}}
+                </div>
+    
+    
+                <div class="modal-footer">
+    
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        
+                        <button class="btn btn-danger">Delete</button>
+                    </form>
+    
+                </div>
+    
+              </div>
+            </div>
+        </div>
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Delete Comic
+        </button>
             <a href="{{route('comics.edit', $comic->id)}}" class="btn btn-primary">edit</a>
     </div>
 </div>
